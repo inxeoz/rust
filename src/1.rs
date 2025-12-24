f9263a3f96e8e9cfd36f2527f9d201f50649468d
inxeoz <inxeoz@inxeoz.com>
2025-12-24 18:33:23 +0530

    for input sum

--- src/1.rs
+++ src/1.rs
@@ -1,7 +1,23 @@
+
 use std::io::{stdin, stdout};
 
 fn main() {
-    let mut input: String = String::new();
-    stdin().read_line(&mut input).expect("did not get it");
-    let n = input.trim().parse::<i32>().expect("invalid input");
+
+    let mut input : String  = String::new();
+
+    stdin().read_line(&mut input).expect("did not get input");
+
+    let n : i32 = input.trim().parse::<i32> ().expect("invalid input");
+
+    let mut sum : i32 = 0;
+
+    for v in 1..=n {
+
+        sum += v;
+
+        println!("{}", v)
+    }
+
+    println!("sum {}", sum)
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{stdin, stdout};
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut input : String  = String::new();
+
+    stdin().read_line(&mut input).expect("did not get input");
+
+    let n : i32 = input.trim().parse::<i32> ().expect("invalid input");
+
+    let mut sum : i32 = 0;
+
+    for v in 1..=n {
+
+        sum += v;
+
+        println!("{}", v)
+    }
+
+    println!("sum {}", sum)
 }
